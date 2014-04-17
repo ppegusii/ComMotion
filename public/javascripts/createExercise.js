9c1c07927d9132b55a22bcc006f26510c961b445
Ryan Szeto <rszeto@umass.edu>
2014-04-17 16:33:01 -0400

    Added names panel for create exercise (not done yet)

--- public/javascripts/createExercise.js
+++ public/javascripts/createExercise.js
@@ -1,25 +1,29 @@
 /**
- * Created by Ryan on 4/16/14.
- */
+* Created by Ryan on 4/16/14.
+*/
 
-    /*
+
 var numNames = 0;
 
 $(function () {
-    var addNameButton = $('#addNameButton');
-    addNameButton.click(function(event) {
-        var name = prompt('New name:');
-        if(name !== '') {
-            console.log('Entered name ' + name);
-            // add name to list of names
-            numNames++;
-            // add name to UI
-            var html = '<li class="btn btn-sm btn-primary" id=name' + numNames + '>' + name + '</li>';
-            $('#enteredNames').append(html);
-        }
-        else
-            console.log('Entered empty name!');
-    });
-});
+   var addNameButton = $('#addNameButton');
+   addNameButton.click(function(event) {
+     var name = prompt('New name:');
+     if(name !== '') {
+         console.log('Entered name ' + name);
+         // add name to list of names
+         numNames++;
+         // add name to UI
+         //var html = '<a class="btn btn-sm btn-primary" id=name' + numNames + '>' + name + '</a>';
+        var html = '<a class="btn btn-sm btn-primary">' + name + '</a>';
+         $('#enteredNames').append(html);
+     }
+     else
+         console.log('Entered empty name!');
+   });
 
-        */
+   var formSubmitButton = $('#saveExerciseButton');
+   formSubmitButton.click(function(event) {
+      // get all the things!
+   });
+});
