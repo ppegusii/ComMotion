93c43852bdeb5655c3e3cf0be60cee8f6c2a3de3
Alex <awfilipek@gmail.com>
2014-04-19 19:44:28 -0400

    profile pages are displaying data from database

--- routes/profile.js
+++ routes/profile.js
@@ -1,21 +1,55 @@
 var data = require(process.env.DATA);
 
 exports.profile_about = function(req, res){
-      res.render('profile/about', {title: 'Profile About'});
+	userId = req.session.user.id;
+	  data.userGetById( {id: userId},function afterGet(err, user){
+    res.render('profile/about',
+      {
+        title: 'Profile About',
+        user: user,
+        err: err
+      });
+  });
 };
 
 exports.profile_posts = function(req, res){
-      res.render('profile/myposts', {title: 'Profile Posts'});
+	userId = req.session.user.id;
+	data.userGetById( {id: userId},function afterGet(err, user){
+    res.render('profile/myposts',
+      {
+        title: 'Profile Posts',
+        user: user,
+        err: err
+      });
+  });
 };
-
+/*
 exports.profile_creations = function(req, res){
      res.render('profile/mycreations', {title: 'Profile Creations'});
 };
-
+*/
 exports.profile_followers = function(req, res){
-     res.render('profile/followers', {title: 'Profile Followers'});
+		userId = req.session.user.id;
+	  data.userGetById( {id: userId},function afterGet(err, user){
+    res.render('profile/followers',
+      {
+        title: 'Profile Followers',
+        user: user,
+        err: err
+      });
+  });
+
 };
 
 exports.profile_following = function(req, res){
-     res.render('profile/following', {title: 'Profile Following'});
+	userId = req.session.user.id;
+	data.userGetById( {id: userId},function afterGet(err, user){
+    res.render('profile/following',
+      {
+        title: 'Profile Following',
+        user: user,
+        err: err
+      });
+  });
+
 };
